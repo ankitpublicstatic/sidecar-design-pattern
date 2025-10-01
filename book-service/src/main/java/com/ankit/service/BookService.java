4294763170aa41acf8b63f1c2972ce9ed146392f
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-10-01 19:17:58 +0530

    Initial commit

---
+++ book-service/src/main/java/com/ankit/service/BookService.java
@@ -0,0 +1,21 @@
+package com.ankit.service;
+
+import org.springframework.stereotype.Service;
+import com.ankit.dto.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private List<Book> books = new ArrayList<>();
+
+    public Book addBook(Book book) {
+        books.add(book);
+        return book;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
